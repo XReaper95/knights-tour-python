99d04afb90231377b38687d250a340060e923aaf
luisdt <luis.ernesto951008@gmail.com>
2020-01-16 16:57:50 -0300

    Re-implement board with tkinter.Canvas

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from utils import center_windows
 
 root = tk.Tk()
 root.title("Knight`s tour")
+root.resizable = False
 
-cell_x = 20
-cell_y = 13
-board_size_factor = 4
+cell_size = 45
+board_size_factor = 8
 dark = False
 cells = []
 last_label = None
@@ -22,12 +22,13 @@ for x in range(0, board_size_factor):
         color = '#0e140c' if dark else '#a7ab90'
         cell_name = "({0}, {1})".format(x, y)
 
-        last_label = tk.Canvas(root, bg=color, padx=cell_x, pady=cell_y, name=cell_name, image=img)
+        last_label = tk.Canvas(root, bg=color, width=cell_size, height=cell_size, name=cell_name)
         last_label.grid(row=x, column=y)
 
         cells.append(cell_name)
         dark = not dark
 
+last_label.create_image((cell_size / 2, cell_size / 2), image=img, anchor=tk.CENTER)
 print(cells)
 
 if __name__ == '__main__':
