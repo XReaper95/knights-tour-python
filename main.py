50405bf13d9d3d2b13bb71b733a75d27ff5b302f
luisdt <luis.ernesto951008@gmail.com>
2020-01-16 17:04:59 -0300

    Get cell value dynamically

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ root = tk.Tk()
 root.title("Knight`s tour")
 root.resizable = False
 
-cell_size = 45
+cell_size = 40
 board_size_factor = 8
 dark = False
 cells = []
-last_label = None
+last_cell = None
 img = tk.PhotoImage(file="chess_knight.png")
 
 if board_size_factor < 4:
@@ -22,13 +22,13 @@ for x in range(0, board_size_factor):
         color = '#0e140c' if dark else '#a7ab90'
         cell_name = "({0}, {1})".format(x, y)
 
-        last_label = tk.Canvas(root, bg=color, width=cell_size, height=cell_size, name=cell_name)
-        last_label.grid(row=x, column=y)
+        last_cell = tk.Canvas(root, bg=color, width=cell_size, height=cell_size, name=cell_name)
+        last_cell.grid(row=x, column=y)
 
         cells.append(cell_name)
         dark = not dark
 
-last_label.create_image((cell_size / 2, cell_size / 2), image=img)
+last_cell.create_image((last_cell.winfo_reqwidth() / 2, last_cell.winfo_reqheight() / 2), image=img)
 print(cells)
 
 if __name__ == '__main__':
